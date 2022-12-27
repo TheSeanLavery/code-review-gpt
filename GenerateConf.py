57abbdc3a37e6bb5797d8ff6ab44a54695a528b2
Sean Lavery <TheSeanLavery@gmail.com>
2022-12-27 00:34:49 -0800

    add check for tokens

--- GenerateConf.py
+++ GenerateConf.py
@@ -1,5 +1,7 @@
 import os
 import webbrowser
+import requests
+import openai
 
 # Provide some help to the user
 print("\nYou can obtain your GitHub token at the following URL: https://github.com/settings/tokens")
@@ -17,6 +19,24 @@ if os.path.exists("secrets.conf"):
 # Prompt the user for their GitHub and OpenAI SDK keys
 github_token = input("Enter your GitHub token: ")
 openai_sdk = input("Enter your OpenAI SDK key: ")
+
+# Validate the GitHub token
+api_endpoint = "https://api.github.com/user"
+headers = {"Authorization": f"Bearer {github_token}"}
+response = requests.get(api_endpoint, headers=headers)
+if response.status_code == 200:
+    print("GitHub token is valid")
+else:
+    print("GitHub token is invalid")
+
+# Validate the OpenAI SDK key
+openai.api_key = openai_sdk
+try:
+    models = openai.Model.list()
+    print("OpenAI SDK key is valid")
+except openai.api_errors.ApiError as err:
+    print("OpenAI SDK key is invalid")
+
 
 # Create the contents of the secrets.conf file
 contents = "[DEFAULT]\nGITHUB_TOKEN={}\nOPENAI_SDK={}".format(github_token, openai_sdk)
