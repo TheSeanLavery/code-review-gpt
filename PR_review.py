d3a8d76fa69e0929e03685d99e900610d976f752
Sean Lavery <TheSeanLavery@gmail.com>
2022-12-27 22:44:05 -0800

    add tests

--- PR_review.py
+++ PR_review.py
@@ -48,11 +48,22 @@ if response.status_code == 200:
 
     # Iterate through each pull request
     for pull_request in pull_requests:
+
+        pull_request = requests.get(f"{base_url}/repos/{owner}/{repo}/pulls/{number}", headers=headers)
+
+        if pull_request.status_code == 200:
+            pull_request = pull_request.json()
+        else:
+            logging.error(f"Error getting pull request {number}: {pull_request.status_code} {pull_request.reason}")
+
         # Get the number of the pull request
         number = pull_request["number"]
 
         # Get the list of changes for the pull request
         response = requests.get(f"{base_url}/repos/{owner}/{repo}/pulls/{number}/files", headers=headers)
+
+        
+
 
         # Check the status code to make sure the request was successful
         if response.status_code == 200:
@@ -70,12 +81,14 @@ if response.status_code == 200:
         else:
             logging.error(f"Error getting changes for pull request {number}: {response.status_code} {response.reason}")
 
+        
+
         # Get the title and body of the pull request
         title = pull_request["title"]
         body = pull_request["body"]
 
         # Reset the text variable for each iteration
-        text = " Review this code as if you were a Lead Dev. Decide if its good or what should be changed, be very thourough, call out lines of code and format them in a markdown block and explain what should be changed: \n" + title+ "\n" +body+ "\n" +(new_changes)
+        text = " Review this code as if you were a Lead Dev. Give it a total score out of 100. Decide if its good or what should be changed, be very thourough, call out lines of code and format them in a markdown block and explain what should be changed: \n" + title+ "\n" +body+ "\n" +(new_changes)
 
         # Generate a response using the openai library
         response = openai.Completion.create(
@@ -105,3 +118,30 @@ if response.status_code == 200:
             logging.error(f"Error posting comment on pull request {number}: {response.status_code} {response.reason}")
 else:
     logging.error(f"Error getting pull requests: {response.status_code} {response.reason}")
+
+    import pytest
+from unittest.mock import patch
+
+def test_requests_import():
+    assert "requests" in globals()
+
+def test_secrets_parsed():
+    assert github_token == config["DEFAULT"]["GITHUB_TOKEN"]
+    assert openai_sdk_token == config["DEFAULT"]["OPENAI_SDK"]
+
+def test_headers_and_base_url_initialized():
+    assert base_url == "https://api.github.com"
+    assert headers["Authorization"] == f"Token {github_token}"
+    assert headers["Accept"] == "application/vnd.github+json"
+    assert headers["Content-Type"] == "application/json"
+
+def test_get_pull_requests_successful():
+    assert response.status_code == 200
+
+def test_get_changes_successful():
+    assert response.status_code == 200
+
+def test_openai_call_correct():
+    with patch("openai.Completion.create") as mock_create:
+        openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.7, max_tokens=1000, top_p=1, frequency_penalty=0, presence_penalty=0)
+        mock_create.assert_called_with(model="text-davinci-003", prompt=text, temperature=0.7, max_tokens=1000, top_p=1, frequency_penalty=0, presence_penalty=0)
